614bf1327c2b6241574d92e29b0ac0c334659955
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-17 21:50:43 +0100

    initial

---
+++ src/routes/(cms)/+layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import CMS from '@staticcms/core';
+	import '@staticcms/core/dist/main.css';
+
+	onMount(async () => {
+		CMS.init({
+			config: {
+				backend: {
+					name: 'git-gateway',
+					branch: 'master'
+				},
+				publish_mode: 'editorial_workflow',
+				media_folder: 'static/images/uploads',
+				public_folder: '/images/uploads',
+				collections: [
+					{
+						name: 'blog', // Used in routes, e.g., /admin/collections/blog
+						label: 'Blog', // Used in the UI
+						folder: '_posts/blog', // The path to the folder where the documents are stored
+						create: true, // Allow users to create new documents in this collection
+						slug: '{{year}}-{{month}}-{{day}}-{{slug}}', // Filename template, e.g., yyyy-MM-dd-title.md
+						fields: [
+							// The fields for each document, usually in front matter
+							{ label: 'Layout', name: 'layout', widget: 'hidden', default: 'blog' },
+							{ label: 'Title', name: 'title', widget: 'string' },
+							{ label: 'Publish Date', name: 'date', widget: 'datetime' },
+							{ label: 'Featured Image', name: 'thumbnail', widget: 'image' },
+							{ label: 'Rating (scale of 1-5)', name: 'rating', widget: 'number' },
+							{ label: 'Body', name: 'body', widget: 'markdown' }
+						]
+					}
+				]
+			}
+		});
+
+	});
+</script>
+
+<slot />
